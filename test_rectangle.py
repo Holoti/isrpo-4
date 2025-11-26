1d68240f316294e40d8d5c12b6ae35fd43dfd490
Holoti <holoti107@gmail.com>
2025-11-26 17:46:25 +0300

    test: rectangle

---
+++ test_rectangle.py
@@ -0,0 +1,26 @@
+import unittest
+
+import rectangle
+
+class RectangleAreaTestCase(unittest.TestCase):
+	def test_area_zero(self):
+		res = rectangle.area(10, 0)
+		self.assertEqual(res, 0)
+	
+	def test_square(self):
+		res = rectangle.area(10, 10)
+		self.assertEqual(res, 100)
+
+
+class RectanglePerimeterTestCase(unittest.TestCase):
+	def test_perimeter_zero(self):
+		res = rectangle.perimeter(0, 0)
+		self.assertEqual(res, 0)
+	
+	def test_one_side_zero(self):
+		res = rectangle.perimeter(10, 0)
+		self.assertEqual(res, 20)
+	
+	def test_square(self):
+		res = rectangle.perimeter(10, 10)
+		self.assertEqual(res, 40)
