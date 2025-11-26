5b8dba0fc3a9eec75f84051ae46a84129f6338bf
Holoti <holoti107@gmail.com>
2025-11-26 17:46:37 +0300

    test: triangle

---
+++ test_triangle.py
@@ -0,0 +1,29 @@
+import unittest
+
+import triangle
+
+class TriangleAreaTestCase(unittest.TestCase):
+	def test_base_zero(self):
+		res = triangle.area(0, 10)
+		self.assertEqual(res, 0)
+	
+	def test_height_zero(self):
+		res = triangle.area(5, 0)
+		self.assertEqual(res, 0)
+	
+	def test_area_one(self):
+		res = triangle.area(1, 2)
+		self.assertEqual(res, 1)
+	
+	def test_area_float(self):
+		res = triangle.area(3, 5)
+		self.assertEqual(res, 7.5)
+
+class TrianglePerimeterTestCase(unittest.TestCase):
+	def test_equal_sides(self):
+		res = triangle.perimeter(1, 1, 1)
+		self.assertEqual(res, 3)
+	
+	def test_right_triangle(self):
+		res = triangle.perimeter(3, 4, 5)
+		self.assertEqual(res, 12)
